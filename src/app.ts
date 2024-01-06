426a6c2e9068e5d476f333d767ae9e0b2f28412a
Shubham <shubham16394@gmail.com>
2024-01-07 00:49:52 +0530

    moved public folder outside

--- src/app.ts
+++ src/app.ts
@@ -52,7 +52,7 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/dist/coinly')));
+app.use(express.static(path.join(__dirname, '../../public/dist/coinly')));
 
 async function createAndStartServer() {
   connection = await setupDBConnection();
