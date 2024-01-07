8caad6a7dcf633bf2d9596d3d5525984ab95a5fc
Shubham <shubham16394@gmail.com>
2024-01-07 12:31:16 +0530

    added allow header of cors

--- src/app.ts
+++ src/app.ts
@@ -42,12 +42,24 @@ let baseUrl = "/coinly";
 
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: 'https://www.coinly.cash',
-    credentials: true,
-  })
-);
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+   } else {
+       next();
+   }
+});
+
+// app.use(
+//   cors({
+//     origin: 'https://www.coinly.cash',
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
